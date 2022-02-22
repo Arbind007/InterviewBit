ea2316aeb710951c7f19279b3eec2838229d54e0
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-02-22 20:01:23 +0530

    Create Digital Root.cpp

---
+++ math/Digital Root.cpp
@@ -0,0 +1,11 @@
+int Solution::solve(int A) {
+    while(A > 9){
+        int temp = 0;
+        while(A!=0){
+            temp += A % 10;
+            A/=10;
+        }
+        A = temp;
+    }
+    return A;
+}
